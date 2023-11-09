4426ba71681b5653e3db430f3aec311b685d8c4d
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-11-09 19:26:06 +0300

    Implement CQRS with MediatR

--- src/rentACar/RentACar/WebApi/Program.cs
+++ src/rentACar/RentACar/WebApi/Program.cs
@@ -1,11 +1,15 @@
+using Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
