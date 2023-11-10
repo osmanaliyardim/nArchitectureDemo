e40d960411705556bc373709dba7a3ec22765378
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-11-10 19:03:39 +0300

    Implement Repositories and Paging

---
+++ src/rentACar/RentACar/Application/Services/Repositories/IBrandRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+using Core.Persistance.Repositories;
+
+namespace Application.Services.Repositories;
+
+public interface IBrandRepository : IAsyncRepository<Brand>, IRepository<Brand>
+{
+    
+}
