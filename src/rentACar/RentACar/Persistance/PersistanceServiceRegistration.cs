fb6cf6c0a98cda48c491835cd608332a262ce958
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-11-13 15:12:16 +0300

    Implement Persistance layer

---
+++ src/rentACar/RentACar/Persistance/PersistanceServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Application.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistance.Contexts;
+using Persistance.Repositories;
+
+namespace Persistance;
+
+public static class PersistanceServiceRegistration
+{
+    public static IServiceCollection AddPersistanceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("nArchitectureDB"));
+        services.AddScoped<IBrandRepository, BrandRepository>();
+
+        return services;
+    }
+}
