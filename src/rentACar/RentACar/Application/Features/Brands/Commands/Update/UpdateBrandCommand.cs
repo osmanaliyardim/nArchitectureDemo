953faa9e0c6e53309a88d4e04174115153cf5c4e
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-11-16 14:31:11 +0300

    Implement Update and Delete commands

---
+++ src/rentACar/RentACar/Application/Features/Brands/Commands/Update/UpdateBrandCommand.cs
@@ -0,0 +1,37 @@
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.Update;
+
+public class UpdateBrandCommand : IRequest<UpdatedBrandResponse>
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public class UpdateBrandCommandHandler : IRequestHandler<UpdateBrandCommand, UpdatedBrandResponse>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UpdatedBrandResponse> Handle(UpdateBrandCommand request, CancellationToken cancellationToken)
+        {
+            var brandToUpdate = await _brandRepository.GetAsync(predicate: b => b.Id == request.Id, cancellationToken: cancellationToken);
+
+            brandToUpdate = _mapper.Map(request, brandToUpdate);
+
+            await _brandRepository.UpdateAsync(brandToUpdate);
+
+            var response = _mapper.Map<UpdatedBrandResponse>(brandToUpdate);
+
+            return response;
+        }
+    }
+}
---
+++ src/rentACar/RentACar/Application/Features/Brands/Queries/GetById/GetByIdBrandQuery.cs
@@ -0,0 +1,32 @@
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Brands.Queries.GetById;
+
+public class GetByIdBrandQuery : IRequest<GetByIdBrandResponse>
+{
+    public Guid Id { get; set; }
+
+    public class GetByIdBrandQueryHandler : IRequestHandler<GetByIdBrandQuery, GetByIdBrandResponse>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public GetByIdBrandQueryHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetByIdBrandResponse> Handle(GetByIdBrandQuery request, CancellationToken cancellationToken)
+        {
+            //var brand = await _brandRepository.GetAsync(b => b.Id == request.Id, cancellationToken: cancellationToken);
+            var brand = await _brandRepository.GetAsync(b => b.Id == request.Id, withDeleted: true, cancellationToken: cancellationToken);
+
+            GetByIdBrandResponse response = _mapper.Map<GetByIdBrandResponse>(brand);
+
+            return response;
+        }
+    }
+}
