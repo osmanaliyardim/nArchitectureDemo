aef043e8af9ef9901e9e13bbf6fc073d29bc1fbb
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-11-17 19:22:55 +0300

    Implement Dynamic Query support

--- src/corePackages/Core.Packages/Core.Persistance/Dynamic/Filter.cs
+++ src/corePackages/Core.Packages/Core.Persistance/Dynamic/Filter.cs
@@ -10,7 +10,7 @@ public class Filter
 
     public string? Logic { get; set; }
 
-    public IEnumerable<Filter> Filters { get; set; }
+    public IEnumerable<Filter>? Filters { get; set; }
 
     public Filter()
     {
---
+++ src/rentACar/RentACar/Application/Features/Models/Profiles/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
+using AutoMapper;
+using Core.Application.Responses;
+using Core.Persistance.Paging;
+using Domain.Entities;
+
+namespace Application.Features.Models.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Model, GetListModelListItemDto>().ReverseMap();
+        CreateMap<Paginate<Model>, GetListResponse<GetListModelListItemDto>>().ReverseMap();
+
+        CreateMap<Model, GetListByDynamicModelListItemDto>().ReverseMap();
+        CreateMap<Paginate<Model>, GetListResponse<GetListByDynamicModelListItemDto>>().ReverseMap();
+    }
+}
