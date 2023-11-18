9bfe686d696a1b941e25fed69f20a69b4bba2405
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-11-18 14:12:38 +0300

    Implement Business Rules

---
+++ src/rentACar/RentACar/Application/Features/Brands/Constants/BrandsMessages.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Features.Brands.Constants;
+
+public class BrandsMessages
+{
+    public const string AlreadyExists = $"This {nameof(Brand)} already exists!";
+}
