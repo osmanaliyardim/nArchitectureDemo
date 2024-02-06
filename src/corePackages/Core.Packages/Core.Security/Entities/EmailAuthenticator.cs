8adce0a11639817513b0f5c9c7336d80ccd1a634
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-02-06 12:10:19 +0300

    Create Entities for RefreshToken and Otp

---
+++ src/corePackages/Core.Packages/Core.Security/Entities/EmailAuthenticator.cs
@@ -0,0 +1,32 @@
+﻿using Core.Persistance.Repositories;
+
+namespace Core.Security.Entities;
+
+public class EmailAuthenticator : Entity<int>
+{
+    public int UserId { get; set; }
+
+    public string? ActivationKey { get; set; }
+
+    public bool IsVerified { get; set; }
+
+    public virtual User User { get; set; } = null!;
+
+    public EmailAuthenticator()
+    {
+        
+    }
+
+    public EmailAuthenticator(int userId, bool ısVerified)
+    {
+        UserId = userId;
+        IsVerified = ısVerified;
+    }
+
+    public EmailAuthenticator(int id, int userId, bool ısVerified) : base(id)
+    {
+        Id = id;
+        UserId = userId;
+        IsVerified = ısVerified;
+    }
+}
