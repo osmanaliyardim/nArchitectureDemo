331aedbcc45e933b266acb2ec729c5061fc46572
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-02-06 12:53:03 +0300

    Implement Access and Refresh Tokens

---
+++ src/corePackages/Core.Packages/Core.Security/JWT/TokenOptions.cs
@@ -0,0 +1,32 @@
+﻿namespace Core.Security.JWT;
+
+public class TokenOptions
+{
+    public string Audience { get; set; }
+
+    public string Issuer { get; set; }
+
+    public int AccessTokenExpiration { get; set; }
+
+    public string SecurityKey { get; set; }
+
+    public int RefrestTokenTTL { get; set; }
+
+    public TokenOptions()
+    {
+        Audience = string.Empty;
+        Issuer = string.Empty;
+        SecurityKey = string.Empty;
+    }
+
+    public TokenOptions(
+        string audience, string ıssuer, int accessTokenExpiration, 
+        string securityKey, int refrestTokenTTL)
+    {
+        Audience = audience;
+        Issuer = ıssuer;
+        AccessTokenExpiration = accessTokenExpiration;
+        SecurityKey = securityKey;
+        RefrestTokenTTL = refrestTokenTTL;
+    }
+}
