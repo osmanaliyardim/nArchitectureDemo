b56af2f2d4f4bd8c312327785433964238246865
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-02-07 09:27:24 +0300

    Implement OtpNet

---
+++ src/corePackages/Core.Packages/Core.Security/OtpAuthenticator/IOtpAuthenticatorHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Security.OtpAuthenticator;
+
+public interface IOtpAuthenticatorHelper
+{
+    Task<byte[]> GenerateSecretKey();
+
+    Task<string> ConvertSecretKeyToString(byte[] secretKey);
+
+    Task<bool> VerifyCode(byte[] secretKey, string code);
+}
