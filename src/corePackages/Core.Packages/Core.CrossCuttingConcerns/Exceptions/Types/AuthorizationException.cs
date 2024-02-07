aa9f84c43d8e64543377b74c6b4211594c1dfc62
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-02-07 11:10:23 +0300

    Add Authorization Behavior

---
+++ src/corePackages/Core.Packages/Core.CrossCuttingConcerns/Exceptions/Types/AuthorizationException.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Types;
+
+public class AuthorizationException : Exception
+{
+    public AuthorizationException()
+    {
+
+    }
+
+    public AuthorizationException(string? message) : base(message)
+    {
+
+    }
+
+    public AuthorizationException(string? message, Exception? innerException) : base(message, innerException)
+    {
+
+    }
+
+    protected AuthorizationException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+
+    }
+}
